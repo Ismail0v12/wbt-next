f20fad111cd798103773523223e57dcb46c23efc
Ismail0v12 <orifisamilov12@mail.ru>
2022-05-24 22:34:01 +0500

    Made some changes

--- src/modules/detail/index.tsx
+++ src/modules/detail/index.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BaseNavigation } from "../../components/base-navigation";
 import { Footer } from "../../components/footer";
 import { ParticlesAnime } from "../../components/particles-anime";
+import Spinner from "../../components/spinner";
 import { ProductInterface } from "../../Interfaces/ProductInterface";
 
 import styles from "./style.module.css";
@@ -11,14 +12,25 @@ interface DetailPageProps {
 }
 
 function DetailPage({ data }: DetailPageProps) {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const iframeDoc = document.getElementById(
+      "detail-page__iframe"
+    ) as HTMLIFrameElement;
+    if (iframeDoc.contentDocument?.readyState === "complete") {
+      setLoading(false);
+    }
+  }, []);
   return (
     <div className={styles["detail-page"]}>
       <ParticlesAnime />
       <BaseNavigation />
+      {loading && <Spinner />}
       <iframe
         src={data?.link}
         id="detail-page__iframe"
         className={styles["detail-page__iframe"]}
+        allowFullScreen
       />
       <Footer />
     </div>
