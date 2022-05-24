1989e8a639c6c48a1136b9e92a42b96e9928f901
Ismail0v12 <orifisamilov12@mail.ru>
2022-05-24 21:11:23 +0500

    Add new feature

--- src/modules/detail/index.tsx
+++ src/modules/detail/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import ChevronRightIcon from "../../components/assets/icons/ChevronRightIcon";
 import { BaseNavigation } from "../../components/base-navigation";
+import { Footer } from "../../components/footer";
 import { ParticlesAnime } from "../../components/particles-anime";
 import { ProductInterface } from "../../Interfaces/ProductInterface";
-import { DetailPageCarousel } from "./detail-carousel";
+
 import styles from "./style.module.css";
 
 interface DetailPageProps {
@@ -11,39 +11,17 @@ interface DetailPageProps {
   readonly translations: any;
 }
 
-function DetailPage({ data, translations }: DetailPageProps) {
-  console.log(data);
+function DetailPage({ data }: DetailPageProps) {
   return (
     <div className={styles["detail-page"]}>
       <ParticlesAnime />
       <BaseNavigation />
-      <div className="container">
-        <div className={styles["detail-page__wrapper"]}>
-          <DetailPageCarousel dataDetail={data} />
-          <div className={styles["detail-page__body"]}>
-            <div className={styles["detail-page__header"]}>
-              <h2>{data?.title}</h2>
-              <span>
-                {translations?.views}: {data?.views}
-              </span>
-            </div>
-            <article className={styles["detail-page__desciption"]}>
-              {data?.description}
-            </article>
-            <a
-              href={data?.link}
-              target="_blank"
-              rel="noreferrer"
-              className="button"
-            >
-              <span>
-                {translations?.see_more}
-                <ChevronRightIcon />
-              </span>
-            </a>
-          </div>
-        </div>
-      </div>
+      <iframe
+        src={data?.link}
+        id="detail-page__iframe"
+        className={styles["detail-page__iframe"]}
+      />
+      <Footer />
     </div>
   );
 }
