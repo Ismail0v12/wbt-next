a9dd6bc1409b6a056828147dda25508568a62ec0
Ismail0v12 <orifisamilov12@mail.ru>
2022-07-07 01:21:46 +0500

    Fixed bug in icon

--- src/components/assets/icons/SentEmailIcon.tsx
+++ src/components/assets/icons/SentEmailIcon.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export function SentEmailIcon() {
   return (
     <svg height="143" width="292" xmlns="http://www.w3.org/2000/svg">
-      <g fill="none" fill-rule="evenodd">
-        <g fill-rule="nonzero">
+      <g fill="none" fillRule="evenodd">
+        <g fillRule="nonzero">
           <path
             d="m72 51c2 0 3-2 4-4l4-4 4-2 2-3 1-3c0-2 0-5 2-7l2-4-2-4-4-2h-5l-8-3-6 1c-3 1-4 4-5 7l-3 8-3 10 2 5c3 6 9 5 10 5z"
             fill="var(--yellow)"
@@ -34,93 +34,93 @@ export function SentEmailIcon() {
         <path
           d="m63 88c-4 10-13 19-20 23"
           stroke="var(--black)"
-          stroke-width=".6"
+          strokeWidth=".6"
         />
-        <path d="m37 67-1 1-1 1 2 4 1 1h1" stroke="#f8db62" stroke-width=".6" />
+        <path d="m37 67-1 1-1 1 2 4 1 1h1" stroke="#f8db62" strokeWidth=".6" />
         <path
           d="m79 23-2 2-11 6-4 5c0 7 5 13 12 14 5 1 9-2 10-7l1-5c1-6-2-11-6-15m-9 119h-9l-2-2 2-9 8 1-2 6h1l3 2c1 1 0 2-1 2"
           fill="#f8b380"
-          fill-rule="nonzero"
+          fillRule="nonzero"
         />
         <path
           d="m70 142c1 0 2-1 1-2l-3-2h-8l-1 2 2 2z"
           fill="#bdbebd"
-          fill-rule="nonzero"
+          fillRule="nonzero"
         />
         <path
           d="m134 60s38 16 50-6c4-7 2-18-3-20-10-3-13 31 8 29 10-1 23-4 40-22"
           stroke="#FFF"
-          stroke-width=".6"
+          strokeWidth=".6"
         />
         <path
           d="m226 15 25 42c2 2 5 2 6 0l35-56-64 9c-2 0-4 3-2 5"
           fill="var(--yellow)"
-          fill-rule="nonzero"
+          fillRule="nonzero"
         />
         <path
           d="m292 1-64 9c-2 0-4 3-2 5l2 3z"
           fill="white"
-          fill-rule="nonzero"
+          fillRule="nonzero"
         />
         <path
           d="m267 7-26 5a1 1 0 1 1 0-2l26-4h1z"
           fill="#e2e6e7"
-          fill-rule="nonzero"
+          fillRule="nonzero"
         />
-        <path d="m283 7-48 25v15l7-4-3-6z" fill="#ffb013" fill-rule="nonzero" />
-        <path d="m244 27-4-7 13-4 3 5" fill="white" fill-rule="nonzero" />
+        <path d="m283 7-48 25v15l7-4-3-6z" fill="#ffb013" fillRule="nonzero" />
+        <path d="m244 27-4-7 13-4 3 5" fill="white" fillRule="nonzero" />
         <path
           d="m256 16 12-5m-10 7 6-3m-15 21 10-9m-9 11 10-9m-5 16 9-11m-8 13 9-11m-7 13 5-7m-12-2 7-7"
           stroke="var(--black)"
-          stroke-width=".6"
+          strokeWidth=".6"
         />
         <path
           d="m229 13a1 1 0 1 1 -2-1 1 1 0 0 1 2 1"
           fill="#e7655b"
-          fill-rule="nonzero"
+          fillRule="nonzero"
         />
         <path
           d="m232 12a1 1 0 1 1 -2 0 1 1 0 0 1 2 0"
           fill="#f8de70"
-          fill-rule="nonzero"
+          fillRule="nonzero"
         />
         <path
           d="m235 12a1 1 0 1 1 -2-1 1 1 0 0 1 2 1"
           fill="#00c6b6"
-          fill-rule="nonzero"
+          fillRule="nonzero"
         />
-        <path d="m237 11h1m-1 0h-1l1 1" stroke="#bdbebd" stroke-width=".6" />
-        <path d="m263 25 7 9 7-13-4-4z" fill="white" fill-rule="nonzero" />
-        <path d="m235 47 4-10 3 5z" fill="#ffb013" fill-rule="nonzero" />
-        <path d="m292 1-64 9" stroke="#aeaeae" stroke-width=".6" />
-        <path d="m235 46 4-9 6-4" stroke="#ffb013" stroke-width=".6" />
-        <path d="m68 54 1 2 3-1 3 2 1-2" stroke="#c0bebf" stroke-width=".6" />
+        <path d="m237 11h1m-1 0h-1l1 1" stroke="#bdbebd" strokeWidth=".6" />
+        <path d="m263 25 7 9 7-13-4-4z" fill="white" fillRule="nonzero" />
+        <path d="m235 47 4-10 3 5z" fill="#ffb013" fillRule="nonzero" />
+        <path d="m292 1-64 9" stroke="#aeaeae" strokeWidth=".6" />
+        <path d="m235 46 4-9 6-4" stroke="#ffb013" strokeWidth=".6" />
+        <path d="m68 54 1 2 3-1 3 2 1-2" stroke="#c0bebf" strokeWidth=".6" />
         <path
           d="m72 55v4l-1 2c0 3-2 9-4 11l-1 5m12-18c2 4 2 6 1 8l-5 8"
           stroke="var(--yellow)"
-          stroke-width=".6"
+          strokeWidth=".6"
         />
         <path
           d="m47 79c2 1 5 1 7-1l2-2m16 4 1 2 1 1"
           stroke="var(--black)"
-          stroke-width=".6"
+          strokeWidth=".6"
         />
         <path
           d="m4 97 1 1m-1 0v1m64 40 1-2m0 2 1-1"
           stroke="darkgrey"
-          stroke-width=".6"
+          strokeWidth=".6"
         />
         <path
           d="m61 131 8 1m-8-3 8 2m-57-39-3 7m5-7-2 6"
           stroke="var(--black)"
-          stroke-width=".6"
+          strokeWidth=".6"
         />
         <path
           d="m66 46c-3-3 4-5 5-7 2-4 1-10 5-12l7-2-5-4-17 14 2 11zm-65-9v3c5 10 17 19 22 5 9-25 15-1 24-18 4-8 8-8 18-1l2-2c-5-5-11-9-18-10-12-4 0 15-15 10-27-8-18 16-29 14-1 0-3-2-4-1"
           fill="var(--yellow)"
-          fill-rule="nonzero"
+          fillRule="nonzero"
         />
-        <path d="m35 142h47" stroke="#676867" stroke-width=".6" />
+        <path d="m35 142h47" stroke="#676867" strokeWidth=".6" />
       </g>
     </svg>
   );
