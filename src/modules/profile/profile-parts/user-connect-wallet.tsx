e0ea7b7cf9eba3132c8fe5b4ff8f1d927ebfac06
Ismail0v12 <orifisamilov12@mail.ru>
2022-09-09 22:45:01 +0500

    Updated redirect

--- src/modules/profile/profile-parts/user-connect-wallet.tsx
+++ src/modules/profile/profile-parts/user-connect-wallet.tsx
@@ -26,99 +26,96 @@ function UserConnectWallet({ connectTo, setConnect }: UserConnectWalletProps) {
         ReactDOM.createPortal(
           <Modal setModal={setConnect} title={translations?.connect_wallet}>
             <>
-              {userData?.total_refs >= 100 ? (
-                <>
-                  <div className="share">
-                    <button
-                      onClick={() => {
-                        if (status === "unavailable") {
+              <>
+                <div className="share">
+                  <button
+                    onClick={() => {
+                      if (status === "unavailable") {
+                        return;
+                      } else {
+                        if (status === "connected") {
                           return;
                         } else {
-                          if (status === "connected") {
-                            return;
-                          } else {
-                            connect()
-                              .then(async (res) => {
-                                if (res) {
-                                  await putData(`/profile/${userData?.id}/`, {
-                                    metamask_id: res[0],
-                                  }).then((res: any) => {
-                                    if (res) {
-                                      setConnect(false);
-                                      document.location.reload();
-                                    }
-                                  });
-                                }
-                              })
-                              .catch((err) => console.log(err));
-                          }
+                          connect()
+                            .then(async (res) => {
+                              if (res) {
+                                await putData(`/profile/${userData?.id}/`, {
+                                  metamask_id: res[0],
+                                }).then((res: any) => {
+                                  if (res) {
+                                    setConnect(false);
+                                    document.location.reload();
+                                  }
+                                });
+                              }
+                            })
+                            .catch((err) => console.log(err));
                         }
-                      }}
+                      }
+                    }}
+                    className="share__button"
+                  >
+                    {status === "unavailable" && (
+                      <h5>
+                        {walletStatus ? (
+                          <span onClick={() => window.location.reload()}>
+                            {translations?.please_click_me}
+                          </span>
+                        ) : (
+                          <a
+                            href="https://metamask.io/"
+                            target="_blank"
+                            rel="noreferrer"
+                            onClick={() => setWalletStatus(true)}
+                          >
+                            {translations?.please_install_metamask}
+                          </a>
+                        )}
+                      </h5>
+                    )}
+                    {status === "notConnected" && (
+                      <h5>{translations?.connect_to_metamask}</h5>
+                    )}
+                    {status === "connected" && (
+                      <h5>{translations?.metamask_connected}</h5>
+                    )}
+                    {status === "initializing" && (
+                      <h5>{translations?.initializing_metamask}</h5>
+                    )}
+                    {status === "connecting" && (
+                      <h5>{translations?.connecting_to_metamask}</h5>
+                    )}
+                    <img src={metamask.src} alt="whitebridge.club" />
+                  </button>
+                  {status === "connected" && (
+                    <button
                       className="share__button"
+                      onClick={() => {
+                        navigator.clipboard.writeText(userData?.metamask_id);
+                        setCopied(true);
+                      }}
                     >
-                      {status === "unavailable" && (
+                      {status === "connected" && (
                         <h5>
-                          {walletStatus ? (
-                            <span onClick={() => window.location.reload()}>
-                              {translations?.please_click_me}
-                            </span>
-                          ) : (
-                            <a
-                              href="https://metamask.io/"
-                              target="_blank"
-                              rel="noreferrer"
-                              onClick={() => setWalletStatus(true)}
-                            >
-                              {translations?.please_install_metamask}
-                            </a>
-                          )}
+                          {copied
+                            ? translations?.copied
+                            : translations?.click_to_copy}
+                          : {userData?.metamask_id}
                         </h5>
                       )}
-                      {status === "notConnected" && (
-                        <h5>{translations?.connect_to_metamask}</h5>
-                      )}
-                      {status === "connected" && (
-                        <h5>{translations?.metamask_connected}</h5>
-                      )}
-                      {status === "initializing" && (
-                        <h5>{translations?.initializing_metamask}</h5>
-                      )}
-                      {status === "connecting" && (
-                        <h5>{translations?.connecting_to_metamask}</h5>
-                      )}
-                      <img src={metamask.src} alt="whitebridge.club" />
                     </button>
-                    {status === "connected" && (
-                      <button
-                        className="share__button"
-                        onClick={() => {
-                          navigator.clipboard.writeText(userData?.metamask_id);
-                          setCopied(true);
-                        }}
-                      >
-                        {status === "connected" && (
-                          <h5>
-                            {copied
-                              ? translations?.copied
-                              : translations?.click_to_copy}
-                            : {userData?.metamask_id}
-                          </h5>
-                        )}
-                      </button>
-                    )}
-                  </div>
-                  <a
-                    href="https://whitebridge.site/static/images/default_img.png"
-                    className="howto"
-                    download
-                  >
-                    <QuestionMarkIcon />
-                    {translations?.learn_how_to_connect}
-                  </a>
-                </>
-              ) : (
-                <h5>{translations?.not_enough_referrals}</h5>
-              )}
+                  )}
+                </div>
+                <a
+                  href="https://whitebridge.site/static/images/default_img.png"
+                  className="howto"
+                  download
+                  style={{ marginTop: 10, display: "block" }}
+                >
+                  <QuestionMarkIcon />
+                  {translations?.learn_how_to_connect}
+                </a>
+              </>
             </>
           </Modal>,
           document.getElementById("__next") as HTMLDivElement
@@ -128,3 +125,8 @@ function UserConnectWallet({ connectTo, setConnect }: UserConnectWalletProps) {
 }
 
 export { UserConnectWallet };
+
+// : (
+//   <h5>{translations?.not_enough_referrals}</h5>
+// )}
+// {userData?.total_refs >= 1 ?
